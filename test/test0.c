560dccb1dfb20817bfe5764adfd09ec847435255
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-03-17 04:35:27 +0000

    draw a square and a cercle

--- test/test0.c
+++ test/test0.c
@@ -1,6 +1,8 @@
 #include "../minilibx-linux/mlx.h"
-
+#include <math.h>
 #define key_w 66
+#define M_PI 3.14159265358979323846
+#include <stdio.h>
 
 // int	key_hook(void *param)
 // {
@@ -16,15 +18,47 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int main()
 {
 	void *mlx;
 	void *mlx_win;
 	t_data img;
+	int j;
+	int i;
 
+	i = 0;
 	mlx = mlx_init();
-	// mlx_win = mlx_new_window(mlx, 1920, 1080, "AYOUB OUAHIDI hHhHhHhHh");
-	mlx_new_image(mlx, 1920, 1080);
-	// mlx_loop(mlx);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "AYOUB OUAHIDI hHhHhHhHh");
+	img.img =mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
+	//square
+	// while (i < 200)
+	// {
+	// 	j = 0;
+	// 	while (j < 200)
+	// 	{
+	// 		my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
+	//cercle 
+	while(i < 360)
+	{
+		printf("hey im here");
+		j = i * (M_PI / 180);
+		my_mlx_pixel_put(&img,960 + (int)(100 * cos(j)),540 + (int)(100 * sin(j)), 0x00FF0000);
+		i++;
+	}
+	
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
