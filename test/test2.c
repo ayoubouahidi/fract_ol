88ddce8c15078a9bce8030c038566a24abfc85ca
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-03-18 01:57:48 +0000

    cercle in test 2 nad test 1

--- test/test2.c
+++ test/test2.c
@@ -1,14 +1,6 @@
 #include "../minilibx-linux/mlx.h"
 #include <math.h>
-#define key_w 66
-#define M_PI 3.14159265358979323846
 #include <stdio.h>
-
-// int	key_hook(void *param)
-// {
-// 	if (key_w == param)
-
-// }
 
 typedef struct	s_data {
 	void	*img;
@@ -30,34 +22,35 @@ int main()
 {
 	void *mlx;
 	void *mlx_win;
+	double	i;
+	double y0 = 500;
+	double x0 = 500;
+	double r = 150;
+	double x;
 	t_data img;
-	int j;
-	int i;
 
-	i = 0;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "AYOUB OUAHIDI hHhHhHhHh");
 	img.img =mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
-	//square
-	// while (i < 200)
-	// {
-	// 	j = 0;
-	// 	while (j < 200)
-	// 	{
-	// 		my_mlx_pixel_put(&img, i, j, 0x00FF0000);
-	// 		j++;
-	// 	}
-	// 	i++;
-	// }
-	//cercle 
-	while(i < 360)
+
+	i = y0 -r;
+	while (i <= y0 + r)
 	{
-		j = i * (M_PI / 180);
-		my_mlx_pixel_put(&img,960 + (int)(100 * cos(j)),540 + (int)(100 * sin(j)), 0x00FF0000);
-		i++;
+		// x = sqrt(r * r - (i - y0) * (i - y0)) + x0;
+		// my_mlx_pixel_put(&img, x, i, 0x00FF0000);	
+		// printf("hey");
+		// i = i + 0.1;
+		if (r * r - (i - y0) * (i - y0) >= 0) {
+            x = sqrt(r * r - (i - y0) * (i - y0)) + x0;
+            my_mlx_pixel_put(&img, x, i, 0x00FF0000); 
+
+            x = -sqrt(r * r - (i - y0) * (i - y0)) + x0;
+            my_mlx_pixel_put(&img, x, i, 0x00FF0000);
+        }
+        i = i + 0.1;
 	}
-	
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+	return (0);
 }
